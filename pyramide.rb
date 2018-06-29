22327c75c50bf2640b9917d6c6765509b4ef5acd
loundness <ludo.vic@me.com>
2018-06-29 15:32:38 +0200

    rajout de la condition 1 à 25

--- pyramide.rb
+++ pyramide.rb
@@ -4,6 +4,12 @@ chiffre = gets.chomp.to_i
 
 n = chiffre
 
+while chiffre <1 || chiffre >25
+	puts "Veuillez renter un nombre entre 1 et 25"
+chiffre = gets.chomp.to_i
+n = chiffre
+end
+
 puts "Voici la pyramide:"
 
 1.upto(n).each{|n|puts ("#" * n).rjust(chiffre*1)}
