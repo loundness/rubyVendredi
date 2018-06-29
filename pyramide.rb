4c88ac0b5658c1d289da100eccc5d7e27ddbc63c
loundness <ludo.vic@me.com>
2018-06-29 14:18:02 +0200

    modification du fichier ruby

--- pyramide.rb
+++ pyramide.rb
@@ -1,5 +1,10 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+
 chiffre = gets.chomp.to_i
+
 n = chiffre
+
 puts "Voici la pyramide:"
-  1.upto(n).each{|n|puts ("* " * n).rjust(chiffre*2)}
+
+1.upto(n).each{|n|puts ("#" * n).rjust(chiffre*1)}
+
